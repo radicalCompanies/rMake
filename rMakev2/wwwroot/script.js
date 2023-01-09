b8501e0768949799de4a888531a0337311234a3e
WillHelmCoder <55366718+WillHelmCoder@users.noreply.github.com>
2023-01-09 00:34:31 -0700

    Correccion de pelea del focus entre el editor de titulo y los text areas

--- rMakev2/wwwroot/script.js
+++ rMakev2/wwwroot/script.js
@@ -6,17 +6,9 @@ function focusInput() {
 function focusLastTextEditor(id) {
     
     document.getElementById("Area" + id-1);
-
-    const parent = document.querySelector('#Area'+id);
-    console.log("1"+parent); 
-
-    const child1 = parent.querySelector('.rte-editor');
-    console.log("2" + child1); 
-
+    const parent = document.querySelector('#Area'+id);   
+    const child1 = parent.querySelector('.rte-editor'); 
     const child2 = child1.querySelectorAll('.ql-editor');
-    console.log(child2); 
-    console.log(child2[0]); 
-
     child2[0].focus();
 
  
