a54d4c9990254e53a18e120f4f9716eb21cdc3cc
Gardel Otero <gardel.intern@xipetechnology.com>
2023-01-23 12:35:11 -0700

    DeleteProject and DeleteDocument added in view -Sebastian

--- rMakev2/ViewModel/RmakeViewModel.cs
+++ rMakev2/ViewModel/RmakeViewModel.cs
@@ -84,8 +84,8 @@ namespace rMakev2.ViewModel
 
                 Thread.Sleep(10000);
                 HashMyContent();
-                _communicationService.SaveAsync(App).Wait();
-                this._toastService.ShowSuccess("Project Saved");
+                //_communicationService.SaveAsync(App).Wait();
+                //this._toastService.ShowSuccess("Project Saved");
             }
         }
 
